0d3d178100dd3337d5bbfe650826a1dcfe89cb2b
nileshp-07 <patidarnilesh7223@gmail.com>
2024-03-01 16:51:39 +0530

    Adding Starter_packs

---
+++ react-app-tailwind/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center bg-slate-500 text-4xl font-bold ">
+        HELLO REACT APP
+    </div>
+  );
+}
+
+export default App;
